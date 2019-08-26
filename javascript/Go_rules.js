599a3aed6279a1f9ea6952b7b1fa48dacf3d6e61
root <root@localhost.localdomain>
2019-08-26 20:30:23 +0800

    first commit

---
+++ javascript/Go_rules.js
@@ -0,0 +1,88 @@
+/*
+* @Author: root
+* @Date:   2019-04-25 23:45:08
+* @Last Modified by:   root
+* @Last Modified time: 2019-08-26 18:52:22
+*/
+
+// 围棋规则实现
+var eat_white = 0, eat_black = 0;
+var now_x = 0, now_y = 0;
+function liberty(x, y, color) {
+	// 判断所落棋子左方是否非越界且存在异色棋子;
+	if( !Transboundary(x-1, y) && number[x-1][y]==color )
+		if(liberty2(x-1, y, color, flag_left) == true)
+			reset_flag(flag_left);
+	// 判断所落棋子右方是否非越界且存在异色棋子;
+	if( !Transboundary(x+1, y) && number[x+1][y]==color )
+		if(liberty2(x+1, y, color, flag_right) == true)
+			reset_flag(flag_right);
+	// 判断所落棋子上方是否非越界且存在异色棋子;
+	if( !Transboundary(x, y-1) && number[x][y-1]==color )
+		if(liberty2(x, y-1, color, flag_up) == true)
+			reset_flag(flag_up);
+	// 判断所落棋子下方是否非越界且存在异色棋子;
+	if( !Transboundary(x, y+1) && number[x][y+1]==color )
+		if(liberty2(x, y+1, color, flag_down) == true)
+			reset_flag(flag_down);
+	// 提子
+	eat(color, flag_up, flag_down, flag_left, flag_right);
+}
+function liberty2(x, y, color, array_flag) {
+	array_flag[x][y] = color;
+	// 判断该棋子 ‘左、右、上、下’ 是否存在活气，若存在返回true;
+	if( (!Transboundary(x-1, y  ) && number[x-1][y  ]==0) ||
+		(!Transboundary(x+1, y  ) && number[x+1][y  ]==0) ||
+		(!Transboundary(x  , y-1) && number[x  ][y-1]==0) ||
+		(!Transboundary(x  , y+1) && number[x  ][y+1]==0) ){
+		return true;
+	}
+	// 判断 ‘左、右、上、下’ 是否存在同色棋子，若存在则进行递归;
+	if( !Transboundary(x-1, y) && number[x-1][y]==color && array_flag[x-1][y]!=color )
+		return liberty2(x-1, y, color, array_flag);
+
+	if( !Transboundary(x+1, y) && number[x+1][y]==color && array_flag[x+1][y]!=color )
+		return liberty2(x+1, y, color, array_flag);
+
+	if( !Transboundary(x, y-1) && number[x][y-1]==color && array_flag[x][y-1]!=color )
+		return liberty2(x, y-1, color, array_flag);
+
+	if( !Transboundary(x, y+1) && number[x][y+1]==color && array_flag[x][y+1]!=color )
+		return liberty2(x, y+1, color, array_flag);
+}
+// 递归条件判断是否越界
+function Transboundary(x, y) {
+	if(x<0 || x>18 || y<0 || y>18)
+		return true;
+	else 
+		return false;
+}
+// 提子效果，将递归所记录下的可吃棋子记录下来，之后一并提子;
+function eat(color, flag_up, flag_down, flag_left, flag_right) {
+	for(var i=0; i<19; i++) {
+		for(var j=0; j<19; j++) {
+			if( flag_up[j][i]    == color || 
+				flag_down[j][i]  == color || 
+				flag_left[j][i]  == color || 
+				flag_right[j][i] == color ) {
+
+				ctx1.beginPath();
+				ctx1.clearRect(j*30, i*30, 30, 30);
+				number[j][i] = 0;
+				if(color == -1) eat_white++;
+				else eat_black++;
+			}
+			flag_up[j][i]    = 0;
+			flag_down[j][i]  = 0;
+			flag_left[j][i]  = 0;
+			flag_right[j][i] = 0;
+		}
+	}
+}
+function reset_flag(array_flag) {
+	for(var i=0; i<19; i++) {
+		for(var j=0; j<19; j++) {
+			array_flag[j][i] = 0;
+		}
+	}
+}
---
+++ javascript/chessboard.js
@@ -0,0 +1,93 @@
+/*
+* @Author: root
+* @Date:   2019-04-25 23:10:27
+* @Last Modified by:   root
+* @Last Modified time: 2019-08-26 18:27:43
+*/
+var blackImg = new Image();
+var whiteImg = new Image();
+blackImg.src = "image/black.png";
+whiteImg.src = "image/white.png";
+var flag     = 1;
+var number     = [],
+	flag_up    = [],
+	flag_down  = [],
+	flag_left  = [],
+	flag_right = [];
+var ctx, ctx1, canvas, canvas1;
+for(var i=0; i<19; i++) {
+	number[i]     = [];
+	flag_up[i]    = [];
+	flag_down[i]  = [];
+	flag_left[i]  = [];
+	flag_right[i] = [];
+	for(var j=0; j<19; j++) {
+		number[i][j]     = 0;
+		flag_up[i][j]    = 0;
+		flag_down[i][j]  = 0;
+		flag_left[i][j]  = 0;
+		flag_right[i][j] = 0;
+	}
+}
+function show() {
+	console.clear();
+	for(var i=0; i<19; i++) {
+		var s = "";
+		for(var j=0; j<19; j++) {
+			s = s + number[j][i] +"  ";
+		}
+		console.log(s+" "+i);
+	}
+}
+$(document).ready(function(){
+	canvas  = document.getElementById("box");
+	ctx     = canvas.getContext("2d");
+	canvas1 = document.getElementById("box1");
+	ctx1    = canvas1.getContext("2d");
+	for(var i=0; i<19; i++) {	// 绘制围棋棋盘网格
+		ctx.moveTo(15+i*30, 15);
+		ctx.lineTo(15+i*30, 555);
+		ctx.moveTo(15, 15+i*30);
+		ctx.lineTo(555, 15+i*30);
+		ctx.stroke();
+	}
+	function piece(position, x, y, r, color) {
+		position.beginPath();
+		position.arc(15+x*30,15+y*30, r, 0, 2*Math.PI);
+		position.fillStyle = color;
+		position.fill();
+		position.stroke();
+	}
+	piece(ctx, 3 , 3 , 5, "black"); 	// 绘制棋盘上的8个星位、1个天元
+	piece(ctx, 15, 3 , 5, "black");
+	piece(ctx, 3 , 15, 5, "black");
+	piece(ctx, 15, 15, 5, "black");
+	piece(ctx, 9 , 9 , 5, "black");
+	piece(ctx, 3 , 9 , 5, "black");
+	piece(ctx, 9 , 3 , 5, "black");
+	piece(ctx, 15 , 9 , 5, "black");
+	piece(ctx, 9 , 15 , 5, "black");
+
+	canvas1.onclick = function(e) {		// 鼠标点击棋盘落子
+		var x = Math.floor(e.offsetX/30);
+		var y = Math.floor(e.offsetY/30);
+		if(number[x][y] == 0) {
+			s_Regrets.push(x, y, flag++);
+			//show();
+			if(flag%2!=0){
+				piece(ctx1, x, y, 13, "white"); // 刚落子的颜色
+				$("#now").css("background-image", "url('image/black.png')"); // 下一个落子颜色
+				number[x][y] = 1; // 记录落子位置
+				liberty(x, y, -1); // 进入的递归
+				$("#white").html(eat_white); // 表明提子数
+			} else{
+				piece(ctx1, x, y, 13, "black"); // 刚落子的颜色
+				$("#now").css("background-image", "url('image/white.png')"); // 下一个落子颜色
+				number[x][y] = -1; // 记录落子位置
+				liberty(x, y, 1); // 进入的递归
+				$("#black").html(eat_black); // 表明提子数
+			}
+			//show();
+		}
+	}
+});
