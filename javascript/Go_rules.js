8c7ea3a15527da950d1c0c66adc547719caa3da0
f-society <42274759+H-f-society@users.noreply.github.com>
2020-01-05 15:15:55 +0800

    Update Go_rules.js

--- javascript/Go_rules.js
+++ javascript/Go_rules.js
@@ -9,43 +9,73 @@
 var eat_white = 0, eat_black = 0;
 var now_x = 0, now_y = 0;
 function liberty(x, y, color) {
-	// 判断所落棋子左方是否非越界且存在异色棋子;
-	if( !Transboundary(x-1, y) && number[x-1][y]==color )
-		if(liberty2(x-1, y, color, flag_left) == true)
-			reset_flag(flag_left);
-	// 判断所落棋子右方是否非越界且存在异色棋子;
-	if( !Transboundary(x+1, y) && number[x+1][y]==color )
-		if(liberty2(x+1, y, color, flag_right) == true)
-			reset_flag(flag_right);
-	// 判断所落棋子上方是否非越界且存在异色棋子;
-	if( !Transboundary(x, y-1) && number[x][y-1]==color )
-		if(liberty2(x, y-1, color, flag_up) == true)
-			reset_flag(flag_up);
-	// 判断所落棋子下方是否非越界且存在异色棋子;
-	if( !Transboundary(x, y+1) && number[x][y+1]==color )
-		if(liberty2(x, y+1, color, flag_down) == true)
-			reset_flag(flag_down);
-	// 提子
-	eat(color, flag_up, flag_down, flag_left, flag_right);
-}
-function liberty2(x, y, color, array_flag) {
-	array_flag[x][y] = color;
-	// 判断该棋子 ‘左、右、上、下’ 是否存在活气，若存在返回true;
-	if( (!Transboundary(x-1, y  ) && number[x-1][y  ]==0) ||
-		(!Transboundary(x+1, y  ) && number[x+1][y  ]==0) ||
-		(!Transboundary(x  , y-1) && number[x  ][y-1]==0) ||
-		(!Transboundary(x  , y+1) && number[x  ][y+1]==0) ){
-		return true;
+	if( !Transboundary(x-1, y) && number[x-1][y]==color ) {
+		if(!liberty2(x-1, y, color)) {
+			eatChess(color, flag_arr);
+			reset_flag(flag_arr);
+		}
 	}
-	// 判断 ‘左、右、上、下’ 是否存在同色棋子，若存在则进行递归;
-	if( !Transboundary(x-1, y) && number[x-1][y]==color && array_flag[x-1][y]!=color )
-		return liberty2(x-1, y, color, array_flag);
-	if( !Transboundary(x+1, y) && number[x+1][y]==color && array_flag[x+1][y]!=color )
-		return liberty2(x+1, y, color, array_flag);
-	if( !Transboundary(x, y-1) && number[x][y-1]==color && array_flag[x][y-1]!=color )
-		return liberty2(x, y-1, color, array_flag);
-	if( !Transboundary(x, y+1) && number[x][y+1]==color && array_flag[x][y+1]!=color )
-		return liberty2(x, y+1, color, array_flag);
+	if( !Transboundary(x+1, y) && number[x+1][y]==color ) {
+		if(!liberty2(x+1, y, color)) {
+			eatChess(color, flag_arr);
+			reset_flag(flag_arr);
+		}
+	}
+	if( !Transboundary(x, y-1) && number[x][y-1]==color ) {
+		if(!liberty2(x, y-1, color)) {
+			eatChess(color, flag_arr);
+			reset_flag(flag_arr);
+		}
+	}
+	if( !Transboundary(x, y+1) && number[x][y+1]==color ) {
+		if(!liberty2(x, y+1, color)) {
+			eatChess(color, flag_arr);
+			reset_flag(flag_arr);
+		}
+	}
+	reset_flag(flag_arr);
+}
+function liberty2(x, y, color) {
+	var queX = [], queY = [];
+	queX.push(x);
+	queY.push(y);
+	while(queX.length>0 && queY.length>0) {
+		console.log(queX, queY);
+		var i = queX.shift();
+		var j = queY.shift();
+		console.log(i, j, color, queX.length, queY.length);
+		if(Viable(i, j)) {
+			console.log(true);
+			return true;
+		}
+		flag_arr[i][j] = color;
+		if(!Transboundary(i-1, j) && number[i-1][j]==color && flag_arr[i-1][j]!=color) {
+			console.log("left");
+			queX.push(i-1);
+			queY.push(j);
+			flag_arr[i-1][j] = color;
+		}
+		if(!Transboundary(i+1, j) && number[i+1][j]==color && flag_arr[i+1][j]!=color) {
+			console.log("right");
+			queX.push(i+1);
+			queY.push(j);
+			flag_arr[i+1][j] = color;
+		}
+		if(!Transboundary(i, j-1) && number[i][j-1]==color && flag_arr[i][j-1]!=color) {
+			console.log("top");
+			queX.push(i);
+			queY.push(j-1);
+			flag_arr[i][j-1] = color;
+		}
+		if(!Transboundary(i, j+1) && number[i][j+1]==color && flag_arr[i][j+1]!=color) {
+			console.log("down");
+			queX.push(i);
+			queY.push(j+1);
+			flag_arr[i][j+1] = color;
+		}
+	}
+	console.log(false);
+	//return false;
 }
 // 递归条件判断是否越界
 function Transboundary(x, y) {
@@ -54,25 +84,26 @@ function Transboundary(x, y) {
 	else 
 		return false;
 }
+function Viable(x, y) {
+	if( (!Transboundary(x-1, y) && number[x-1][y] == 0) ||
+		(!Transboundary(x+1, y) && number[x+1][y] == 0) ||
+		(!Transboundary(x, y-1) && number[x][y-1] == 0) ||
+		(!Transboundary(x, y+1) && number[x][y+1] == 0) )
+		return true;
+	return false;
+}
 // 提子效果，将递归所记录下的可吃棋子记录下来，之后一并提子;
-function eat(color, flag_up, flag_down, flag_left, flag_right) {
+function eatChess(color, flag_arr) {
 	for(var i=0; i<19; i++) {
 		for(var j=0; j<19; j++) {
-			if( flag_up[j][i]    == color || 
-				flag_down[j][i]  == color || 
-				flag_left[j][i]  == color || 
-				flag_right[j][i] == color ) {
-
+			if(flag_arr[j][i] == color) {
 				ctx1.beginPath();
 				ctx1.clearRect(j*30, i*30, 30, 30);
 				number[j][i] = 0;
 				if(color == -1) eat_white++;
 				else eat_black++;
 			}
-			flag_up[j][i]    = 0;
-			flag_down[j][i]  = 0;
-			flag_left[j][i]  = 0;
-			flag_right[j][i] = 0;
+			flag_arr[j][i] = 0;
 		}
 	}
 }
